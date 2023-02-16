4291c983786eeb4f3316214a62700b37f28861b7
cactus8603 <cactus415646851@gmail.com>
2023-02-16 16:20:24 +0800

    separate parse

--- train.py
+++ train.py
@@ -1,62 +1,78 @@
-import argparse
-import yaml
+
 import torch
 import cv2
+import os
+import math
 import numpy as np
-from torchvision.transforms import Compose, Resize, ToTensor, ToPILImage
-from torchsummary import summary
-from PIL import Image
+import torch.optim.lr_scheduler as lr_scheduler
+from torch.utils.tensorboard import SummaryWriter
+# from torchvision.transforms import Compose, Resize, ToTensor, ToPILImage
 
+from utils.dataset import ImgDataSet
+from utils.utils import read_spilt_data, get_loader, train_one_epoch, evaluate
+from utils.parser import parser_args
 from model.Vit import Vit
 
-def create_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("config_path", default="config.yaml", nargs='?', help="path to config file")
-    
-    args = parser.parse_args()
-    return args
+def train(args_dict):
+    device = torch.device(args_dict if torch.cuda.is_available() else "cpu")
 
-def parser_args():
-    args = create_parser()
-    if args.config_path:
-        with open(args.config_path, 'r') as f:
-            data = yaml.load(f, Loader=yaml.FullLoader)
-        
-        arg_dict = args.__dict__
-        for key, value in data.items():
-            # print(key, value)
-            if isinstance(value, list):
-                for v in value:
-                    arg_dict[key].extend(value)
-            else:
-                arg_dict[key] = value
-    return args
-    
+    train_loader, val_loader = get_loader(args_dict) 
+
+    tb_writer = SummaryWriter()
+
+    model = Vit()
+
+    pg = [p for p in model.parameters() if p.requires_grad]
+    opt = torch.optim.SGD(pg, lr=args_dict['lr'], momentum=args_dict['momentum'], weight_decay=args_dict['weight_decay'])
+    lf = lambda x: ((1 + math.cos(x * math.pi / args_dict['epoch'])) / 2) * (1 - args_dict['lrf']) + args_dict['lrf']
+    scheduler = lr_scheduler.LambdaLR(optimizer=opt, lr_lambda=lf)
+
+    for epoch in range(args_dict['epoch']):
+
+        train_loss, train_acc = train_one_epoch(
+            model=model, 
+            optimizer=opt,
+            data_loader=train_loader,
+            device=device,
+            epoch=epoch
+        )
+
+        val_loss, val_acc = evaluate(
+            model=model, 
+            data_loader=val_loader,
+            device=device,
+            epoch=epoch
+        )
+
+        scheduler.step()
+
+        tags = ["train_loss", "train_acc", "val_loss", "val_acc", "lr"]
+        tb_writer.add_scalar(tags[0], train_loss, epoch)
+        tb_writer.add_scalar(tags[1], train_acc, epoch)
+        tb_writer.add_scalar(tags[2], val_loss, epoch)
+        tb_writer.add_scalar(tags[3], val_acc, epoch)
+        tb_writer.add_scalar(tags[4], opt.param_groups[0]['lr'], epoch)
+
+        if (epoch % 10 == 0):
+            save_path = args_dict['model_save_path'] + "/model_{}.pth".format(epoch)
+            if not os.path.exists(save_path):
+                os.mkdir(save_path)
+            torch.save(model.state_dict(), save_path)
+
+
+
 if __name__ == '__main__':
     args = parser_args()
     args_dict = vars(args)
-    # print(args_dict)
-    # print(args.batch_size)
 
-    img = cv2.imread('1.png')
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = np.zeros_like(img)
-    img[:,:,0] = gray
-    img[:,:,1] = gray
-    img[:,:,2] = gray
 
-    transform = Compose([
-        ToPILImage(),
-        Resize((224, 224)), 
-        ToTensor()
-        ])
-    x = transform(img)
-    x = x.unsqueeze(0)
-    print(x.shape)
+    
+ 
+    for
 
-    model = Vit(args_dict=args_dict)
-    # summary(model(), (1,3,224,224), device='cpu')
-    pred = model(x)
-    print(pred)
+    # model = Vit(args_dict=args_dict)
+    # # summary(model(), (1,3,224,224), device='cpu')
+    # pred = model(x)
+    # print(pred)
 
     
