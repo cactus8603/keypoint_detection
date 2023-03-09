f041c4fe2378e43cc40161eeee47cc37ba87cb5c
cactus8603 <cactus415646851@gmail.com>
2023-03-09 17:45:06 +0800

    add count few chars

--- del_nonword.py
+++ del_nonword.py
@@ -2,20 +2,39 @@ import os
 from tqdm import tqdm
 from glob import glob
 
-# glob all font folder
-font_folder = glob(os.path.join("./image/all_word", '*'))
-# print(font_folder)
+def del_empty_img():
+    # glob all font folder
+    font_folder = glob(os.path.join("./image/all_word", '*'))
+    # print(font_folder)
 
-progress = tqdm(total=len(font_folder))
+    progress = tqdm(total=len(font_folder))
 
-# glob each word in folder
-for font in font_folder:
-    imgs = glob(os.path.join(font, '*'))
-    for img in imgs:
-        stats = os.stat(img)
-        size = stats.st_size
-        if size < 1000:
-            os.remove(img)
-    # os.remove(os.join)
+    # glob each word in folder
+    for font in font_folder:
+        imgs = glob(os.path.join(font, '*'))
+        for img in imgs:
+            stats = os.stat(img)
+            size = stats.st_size
+            if size < 1000:
+                os.remove(img)
+        # os.remove(os.join)
 
-    progress.update(1)
+        progress.update(1)
+
+def count_few_folder():
+    code_folder = glob(os.path.join("../SplitFontFileAugData1", '*'))
+
+    progress = tqdm(total=len(code_folder))
+
+    few_code = []
+    for code in code_folder:
+        imgs = glob(os.path.join(code, '*'))
+        if len(imgs) == 0:
+            few_code.append(code)
+        progress.update(1)
+
+    print(len(few_code))
+
+
+if __name__ == '__main__':
+    count_few_folder()
